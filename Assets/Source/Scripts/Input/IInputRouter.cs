97e5e804927908dffeb27f71d4ddcfe589985a62
Maxim Smolko <Mizzzlo17@gmail.com>
2025-01-11 12:23:53 +0300

    add player input

---
+++ Assets/Source/Scripts/Input/IInputRouter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Source.Scripts.Input
+{
+    public interface IInputRouter
+    {
+        public void OnEnable();
+        public void OnDisable();
+        public Vector2 GetPointerPosition();
+    }
+}
